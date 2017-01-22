01ce7ab308f5be46f35587dfdb9dda49106d1318
nmacawile <nmacawile@gmail.com>
2017-01-23 03:20:33 +0800

    breadth-first search now works

--- binarytrees.rb
+++ binarytrees.rb
@@ -36,23 +36,38 @@ class Node
 		node
 	end
 	
-	#def breadth_first_search
-	#	self.bfs(self)		
-	#end
+	def breadth_first_search
+		array = []		
+		stack = [[self]]
+		loop do
+			temp = []
+			p stack.last
+			stack.last.each do |node|
+				array << node.value	unless node.nil?
+				temp << node.child_a unless node.nil? || node.child_a.nil?	
+				temp << node.child_b unless node.nil? || node.child_b.nil?
+			end
+
+			break if temp.empty?
+			stack << temp
+		end
+
+		array
+	end
 	
-	def self.dfs_rec(tree, arr = [])
+	def self.dfs_rec(tree, arr = [])		# dfs pre-order (root, left, right)
 		return arr if tree.nil?
-		arr << tree.value
-		self.dfs_rec(tree.child_a, arr)
-		self.dfs_rec(tree.child_b, arr)
+		arr << tree.value					#root
+		self.dfs_rec(tree.child_a, arr)		#left
+		self.dfs_rec(tree.child_b, arr)		#right
 		arr
 	end
 		
 end
 
-
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
+#puts "Input array: #{array.inspect}"
 tree = array.build_tree
-#p tree
-p Node.dfs_rec(tree)
+
+#puts "Depth-First Search: #{Node.dfs_rec(tree).inspect}"
+puts "Breadth-First Search: #{tree.breadth_first_search}"
